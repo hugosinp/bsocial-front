31877fe130a2d3bbe864cc17b304623d1512a08a
nlmz94 <nmz94140@gmail.com>
2022-06-22 16:42:45 +0200

    likes

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,7 +1,31 @@
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import Button from './Button';
+import { AiOutlineLike } from 'react-icons/ai';
+import axios from 'axios';
 
 const PostCard = ({ post }) => {
+	const handleSubmit = () => {
+		axios
+			.post(
+				'http://localhost:3001/likes',
+				{
+					post: post._id,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem('token')}`,
+					},
+				}
+			)
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	};
+
 	return (
 		<div className={styles.card}>
 			<div className={styles.header}>
@@ -17,12 +41,19 @@ const PostCard = ({ post }) => {
 					<p className={styles.content}>{post.content}</p>
 				</div>
 			</Link>
-			<div>
+			<div className={styles.bottom}>
 				<span className={styles.date}>
 					{post.createDate.split('T')[0]} {post.createDate.split('T')[1].split('.')[0]}
 				</span>
+				<div className={styles.likes}>
+					<Button variant={'white'} onClick={handleSubmit}>
+						<AiOutlineLike />
+					</Button>
+					<span>{post.likesCount ? post.likesCount : 0}</span>
+				</div>
 			</div>
 		</div>
 	);
 };
+
 export default PostCard;
