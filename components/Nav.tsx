7c14d33bf4764a67a3279b41d292bc59ced67c37
nlmz94 <nmz94140@gmail.com>
2022-06-22 16:14:25 +0200

    better nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,51 +9,54 @@ import {
 	AiOutlineSmile,
 	AiOutlineUser,
 } from 'react-icons/ai';
+import { useRouter } from 'next/router';
 
 const Nav = () => {
+	const router = useRouter();
+	const currentRoute = router.pathname;
 	return (
 		<>
 			<div className={styles.nav}>
 				<div className={styles.link}>
 					<AiOutlineHome />
 					<Link href={'/home'}>
-						<p className={styles.linkText}>Home</p>
+						<p className={currentRoute === '/home' ? styles.linkTextActive : styles.linkTextPassive}>Home</p>
 					</Link>
 				</div>
 				<div className={styles.link}>
 					<AiOutlineSearch />
 					<Link href={'/home'}>
-						<p className={styles.linkText}>Explore</p>
+						<p className={currentRoute === '/search' ? styles.linkTextActive : styles.linkTextPassive}>Explore</p>
 					</Link>
 				</div>
 				<div className={styles.link}>
 					<AiOutlineSmile />
 					<Link href={'/home'}>
-						<p className={styles.linkText}>Communities</p>
+						<p className={currentRoute === '/communities' ? styles.linkTextActive : styles.linkTextPassive}>Communities</p>
 					</Link>
 				</div>
 				<div className={styles.link}>
 					<AiOutlineNotification />
 					<Link href={'/home'}>
-						<p className={styles.linkText}>Notification</p>
+						<p className={currentRoute === '/notifications' ? styles.linkTextActive : styles.linkTextPassive}>Notifications</p>
 					</Link>
 				</div>
 				<div className={styles.link}>
 					<AiOutlineMessage />
 					<Link href={'/home'}>
-						<p className={styles.linkText}>Messages</p>
+						<p className={currentRoute === '/messages' ? styles.linkTextActive : styles.linkTextPassive}>Messages</p>
 					</Link>
 				</div>
 				<div className={styles.link}>
 					<AiOutlineUser />
 					<Link href={'/home'}>
-						<p className={styles.linkText}>Profile</p>
+						<p className={currentRoute === '/profile' ? styles.linkTextActive : styles.linkTextPassive}>Profile</p>
 					</Link>
 				</div>
 				<div className={styles.link}>
 					<AiOutlineSetting />
 					<Link href={'/home'}>
-						<p className={styles.linkText}>Settings</p>
+						<p className={currentRoute === '/settings' ? styles.linkTextActive : styles.linkTextPassive}>Settings</p>
 					</Link>
 				</div>
 			</div>
